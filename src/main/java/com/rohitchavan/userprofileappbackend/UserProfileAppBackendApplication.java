658071164778d82752c44eaa0ffcee9ba9d83e9c
rohit-chavan-2151 <rc2151@gmail.com>
2023-02-05 21:30:07 +0530

    Create post and get request for users

---
+++ src/main/java/com/rohitchavan/userprofileappbackend/UserProfileAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rohitchavan.userprofileappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserProfileAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserProfileAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rohitchavan/userprofileappbackend/UserProfileAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rohitchavan.userprofileappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserProfileAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
